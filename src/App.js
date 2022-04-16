7ba2d0ba697861029bef0fcecebc980cc7f294d0
kavishkhanna <68178046+kavishkhanna@users.noreply.github.com>
2022-04-16 11:42:15 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React33333333333
+          Learn React
         </a>
       </header>
     </div>
